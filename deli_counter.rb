c3f9f44ec6a3d8832e8cf812bd7f4ec441755d8f
mark baruch <mbaruch28@gmail.com>
2019-02-12 19:08:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 # Write your code here.
 katz_deli = ["Ada, Grace, Kent, Matz"]
-def (take_a_number)
+def line (katz_deli)
+  if line.empty?
+  puts "The line is currently empty"
+else line_is_starting
+  puts
+
+  (take_a_number)
   counter=0
 if  puts "Now serving #{katz_deli}{}"
 
